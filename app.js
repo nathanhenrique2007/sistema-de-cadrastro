cfc2e49158622e6260efbfc4dea7993fb221b519
nathanhenrique2007 <127211394+nathanhenrique2007@users.noreply.github.com>
2023-05-30 11:30:34 -0300

    .

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("O paulo fez esseb comit sem voce ver na aula")
+console.log("Envian para o github")
